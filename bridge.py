6659c89be5b3c4caf12987ec4fcc303af46ab50d
Ronit Batchu <ronitbatchu@Ronits-MacBook-Pro.local>
2025-04-27 02:13:13 -0700

    fix

--- bridge.py
+++ bridge.py
@@ -1,7 +1,6 @@
 from web3 import Web3
 from web3.providers.rpc import HTTPProvider
 from web3.middleware import ExtraDataToPOAMiddleware
-from web3.middleware import geth_poa_middleware
 from pathlib import Path
 from datetime import datetime
 import json
@@ -57,7 +56,7 @@ def scan_blocks(chain, contract_info="contract_info.json"):
     else:
         rpc = "https://data-seed-prebsc-1-s1.binance.org:8545/"
     w3 = Web3(Web3.HTTPProvider(rpc))
-    w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+    w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
 
     contract = w3.eth.contract(
         address=w3.toChecksumAddress(addr),
